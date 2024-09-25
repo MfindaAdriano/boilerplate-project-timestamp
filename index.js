79c239c5bb4bf34764a92bef1bcd17d9c6bca8a9
Mfinda Adriano <mfindaadriano@gmail.com>
2024-09-25 18:52:36 +0100

    first commit after checking that the project is working properly

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use(express.urlencoded({extended: false}));
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -23,6 +25,49 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+
+//http://localhost:3000/api/449879709871
+//A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+// 10%11%2018
+
+/*************Starts Here ********* */
+app.get("/api/:date?", (req, res) => {
+  let codificado = encodeURI("12 10 2019");
+  console.log(codificado);
+  console.log(decodeURI(codificado));
+  console.log(new Date(decodeURI(codificado)));
+  console.log(new Date("2022, 12 30"));
+  console.log("Data: " + new Date(decodeURI(codificado)))
+
+
+  let date = req.params.date;
+  let dateObj;
+
+  //if date parameter is empty
+  if(!date){
+    //date = new Date(req.params.date);
+    dateObj = new Date();
+    res.json({unix: dateObj.getTime(), utc: dateObj.toUTCString()});
+  }else{
+      const arg = decodeURI(req.params.date);
+      dateObj = new Date(arg);
+
+      if(new Date(arg).toUTCString() === "Invalid Date"){
+        if(new Date(parseInt(arg)).toUTCString() === "Invalid Date"){
+          res.json({error: "Invalid Date"});
+        }else{
+          dateObj = new Date(parseInt(arg));
+          res.json({unix: dateObj.getTime(), utc: dateObj.toUTCString()});
+        }
+        
+      }else{
+        res.json({unix: dateObj.getTime(), utc: dateObj.toUTCString()});
+      }
+      
+  }
+
+})
 
 
 
